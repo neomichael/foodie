5f53f08b18c6843e202a8de114e02453dc502fb8
GAI243_10_林君翰 <talentmichael123@gmail.com>
2024-10-12 17:40:16 +0800

    Update .gitignore and remove ignored files

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'entrance_page.dart';
+import 'photo_taking.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+  static void setLocale(BuildContext context, Locale newLocale) {
+    _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
+    state?.setLocale(newLocale);
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale = Locale('zh', 'TW');
+
+  void setLocale(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Menu Photography App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      locale: _locale,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('zh', 'TW'),
+        Locale('zh', 'CN'),
+        Locale('ja', ''),
+        Locale('ko', ''),
+        Locale('zh', ''),
+      ],
+      home: EntrancePage(),
+      routes: {
+        '/camera': (context) => PhotoTakingScreen(),
+      },
+    );
+  }
+}
